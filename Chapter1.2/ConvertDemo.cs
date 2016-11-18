924466f4009d0cfe3c139e931e64822f69dfdfeb
sathishk2connect <sathishk2connect@gmail.com>
2016-11-18 23:09:31 +0800

    commit

---
+++ Chapter1.2/ConvertDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1._2
+{
+    class ConvertDemo
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Int32.Parse(null));
+
+            int value = Convert.ToInt32("42");
+                value = int.Parse("42");
+            bool success = int.TryParse(null, out value);
+            Console.WriteLine(success);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter2.5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+using System.Reflection;
+
+
+namespace Chapter2._5
+{
+    [DataContract(Name="Myclass")]
+    class MyClass
+    {
+        private int myRollNo;
+        private int myValues;
+
+        public int MyRollNo { get; set; }
+        public int MyValues { get; set; }
+    }
+
+    [Helper]
+    class HelperClass2
+    {
+
+    }
+
+    [Helper]
+    class HelperClass1
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+           // Console.WriteLine(Attribute.IsDefined(typeof(MyClass), typeof(SerializableAttribute)));
+           // DataContractAttribute dA = (DataContractAttribute)Attribute.GetCustomAttribute(typeof(MyClass), typeof(DataContractAttribute));
+            
+            // Console.WriteLine(dA.Name);
+
+            /*
+            foreach (FieldInfo f in typeof(MyClass).GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance))
+            {
+                Console.WriteLine(f.Name);
+            }
+            foreach(PropertyInfo f in typeof(MyClass).GetProperties())
+            {
+                Console.WriteLine(f.Name);
+            }
+             * */
+
+             
+          
+            Console.ReadKey();
+        }
+    }
+}
